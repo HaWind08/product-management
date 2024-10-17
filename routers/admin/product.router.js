96c1724f833377a6392c9251e08981750cc45061
Ngo Quang Ha <ngoquangha0811xxz@gmail.com>
2024-10-17 10:50:24 +0700

    sắp xếp theo tiêu chí

--- routers/admin/product.router.js
+++ routers/admin/product.router.js
@@ -30,7 +30,13 @@ router.post(
 
 router.get("/edit/:id", controller.edit);
 
-router.patch("/edit/:id", upload.single('thumbnail'), validate.creatPost, controller.editPatch);
+router.patch(
+  "/edit/:id",
+  upload.single('thumbnail'),
+  uploadCloud.upload,
+  validate.creatPost,
+  controller.editPatch
+);
 
 router.get("/detail/:id", controller.detail);
 
