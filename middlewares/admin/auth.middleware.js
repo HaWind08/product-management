2994885dc2b02f6ef144efc566c646103ec482aa
Ngo Quang Ha <ngoquangha0811xxz@gmail.com>
2024-11-02 14:42:24 +0700

    authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 // Tạo router private
 const Account = require("../../models/accounts.model");
+const Role = require("../../models/roles.model");
 const systemConfig = require("../../config/system");
 
 module.exports.requireAuth = async (req, res, next) => {
@@ -7,10 +8,17 @@ module.exports.requireAuth = async (req, res, next) => {
     if (!req.cookies.token) {
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
-        const user = await Account.findOne({ token: req.cookies.token }); // kiểm tra token của user
+        const user = await Account.findOne({ token: req.cookies.token }).select("-password"); // kiểm tra token của user
         if (!user) {
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
         } else {
+            const role = await Role.findOne({
+                _id: user.role_id
+            }).select("title permissions");
+
+            //Trả thông tin user ra giao diện (bất kỳ file pug nào cũng có)
+            res.locals.user = user;
+            res.locals.role = role;
             next();
         };
     };
