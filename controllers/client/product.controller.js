34701a132561f19344e71f4dd3ff22be18eea931
Ngo Quang Ha <ngoquangha0811xxz@gmail.com>
2024-11-03 17:00:22 +0700

    Danh sach san pham client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,7 +1,9 @@
 // Nhúng model
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 
 const productsHelper = require("../../helpers/product");
+const productsCategoryHelper = require("../../helpers/product-category");
 
 // [GET] /products 
 module.exports.index = async (req, res) => {
@@ -15,11 +17,11 @@ module.exports.index = async (req, res) => {
     // console.log(products);
 
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Danh sach san pham",
-        // render product
+        pageTitle: "Danh sách sản phẩm",
+        // --> view (index.pug - products)
         products: newProduct
     });
-}
+};
 
 // [GET] /products/:slug
 module.exports.detail = async (req, res) => {
@@ -39,4 +41,34 @@ module.exports.detail = async (req, res) => {
     } catch (error) {
         res.redirect("/products");
     };
-}
+};
+
+// [GET] /products/:slugCategory 
+module.exports.category = async (req, res) => {
+    // console.log(req.params.slugCategory); //slugCategory: biến đặt bên router
+
+    const category = await ProductCategory.findOne({
+        slug: req.params.slugCategory,
+        status: "active",
+        deleted: false
+    });
+
+    const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+    const listSubCategoryId = listSubCategory.map(item => item.id);
+
+    // console.log(category.id);
+    const products = await Product.find({
+        product_category_id: { $in: [category.id, ...listSubCategoryId] },
+        deleted: false
+    }).sort({ position: "desc" });
+    // console.log(products);
+
+    const newProduct = productsHelper.priceNewProducts(products);
+
+    res.render("client/pages/products/index.pug", {
+        pageTitle: category.title,
+        // --> view (index.pug - products)
+        products: newProduct
+    });
+};
+
--- routers/client/product.router.js
+++ routers/client/product.router.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const controller = require("../../controllers/client/product.controller");
 
-// Đặt async trước hàm
 router.get('/', controller.index);
 
-router.get('/:slug', controller.detail);
+router.get("/:slugCategory", controller.category);
+
+// router.get('/:slug', controller.detail);
 
 module.exports = router;
