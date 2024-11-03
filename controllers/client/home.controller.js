9dc536a5c1532579e20bb140e7c1396e95d1fafb
Ngo Quang Ha <ngoquangha0811xxz@gmail.com>
2024-11-03 07:58:29 +0700

    product category client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,8 @@
-module.exports.index = (req, res) => {
+
+// [GET] /
+module.exports.index = async (req, res) => {
     res.render("client/pages/home/index.pug", {
-        pageTitle: "Trang chu"
+        pageTitle: "Trang chủ",
+        // --> view (index.pug - home)
     });
 }
--- routers/client/index.router.js
+++ routers/client/index.router.js
@@ -2,13 +2,15 @@ const homeRouter = require("./home.router");
 const productRouter = require("./product.router");
 const userRouter = require("./user.router");
 const userMiddleware = require("../../middlewares/client/user.middleware");
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
 
 module.exports = (app) => {
     app.use(userMiddleware.infoUser);
+    app.use(categoryMiddleware.category);
 
     app.use('/', homeRouter);
-    
+
     app.use('/products', productRouter);
 
     app.use('/user', userRouter);
