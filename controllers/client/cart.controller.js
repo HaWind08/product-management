a616683eab466834059497b16ddff4849da08156
Ngo Quang Ha <ngoquangha0811xxz@gmail.com>
2024-11-04 21:07:48 +0700

    cart page

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,41 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+
+const productHelper = require("../../helpers/product");
+
+
+// [GET] /cart/
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const cart = await Cart.findOne({
+        _id: cartId
+    });
+    // console.log(cart);
+
+    if (cart.products.length > 0) {
+        for (const item of cart.products) {
+            const productId = item.product_id
+            const productInfo = await Product.findOne({
+                _id: productId,
+
+            }).select("title thumbnail slug price discountPercentage");
+
+            productInfo.priceNew = productHelper.priceNewProduct(productInfo);
+
+            item.productInfo = productInfo;
+            item.totalPrice = productInfo.priceNew * item.quantity;
+            // console.log(productInfo);
+        };
+    };
+
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0);
+
+    res.render("client/pages/cart/index.pug", {
+        pageTitle: "Giỏ hàng",
+        // --view (index.pug - cart)
+        cartDetail: cart
+    });
+};
 
 // [POST] /cart/add/:productId
 module.exports.add = async (req, res) => {
@@ -19,8 +56,8 @@ module.exports.add = async (req, res) => {
         // Cập nhật lại
         const quantityNew = quantity + existProductInCart.quantity;
         await Cart.updateOne(
-            {_id: cartId, "products.product_id": productId},
-            {$set: {'products.$.quantity': quantityNew}}
+            { _id: cartId, "products.product_id": productId },
+            { $set: { 'products.$.quantity': quantityNew } }
         )
     } else {
         const objectCart = {
--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -3,13 +3,13 @@ const Cart = require("../../models/cart.model");
 // Thêm giỏ hàng không cần đăng nhập
 module.exports.cartId = async (req, res, next) => {
     // console.log(req.cookies.cartId);
-    
+
     if (!req.cookies.cartId) {
         // Tạo giỏ hàng
         const cart = new Cart();
         await cart.save();
         console.log(cart);
-        
+
         const expiresCookies = 365 * 24 * 60 * 60 * 1000;
         res.cookie("cartId", cart.id, { expires: new Date(Date.now() + expiresCookies) });
     } else {
