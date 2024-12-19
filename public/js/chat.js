47c447c67f7c7cdf01f1ec432327b32e3f6fd647
Ngo Quang Ha <ngoquangha0811xxz@gmail.com>
2024-12-19 20:10:42 +0700

    insert icon

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
 if (formSendData) {
@@ -41,3 +43,25 @@ const bodyChat = document.querySelector(".chat .inner-body");
 if(bodyChat) {
     bodyChat.scrollTop = bodyChat.scrollHeight;
 }
+
+// Icon chat
+const buttonIcon = document.querySelector(".button-icon");
+if(buttonIcon) {
+    const tooltip = document.querySelector('.tooltip');
+    Popper.createPopper(buttonIcon, tooltip);
+
+    buttonIcon.onclick = () => {
+        tooltip.classList.toggle("shown");
+    };
+}
+
+// Insert icon in input
+const emojiPicker = document.querySelector("emoji-picker");
+if(emojiPicker) {
+    const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+    emojiPicker.addEventListener("emoji-click", (event) => {
+        const icon = event.detail.unicode;
+        inputChat.value = inputChat.value + icon;
+    });
+}
+
